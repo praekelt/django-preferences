3b0ec9691df1b96e3ec19e2ff5440c12871cedb8
shaunsephton <shaunsephton@gmail.com>
2010-05-12 12:03:24 +0200

    properly hook up options

--- options/models.py
+++ options/models.py
@@ -10,7 +10,9 @@ def options_class_prepared(sender, *args, **kwargs):
     cls = sender
    
     if issubclass(cls, Options):
+        # add singleton manager to subclasses
         cls.add_to_class('singleton', SingletonManager())
-        #setattr(options, cls._meta.object_name, cls.singleton.get())
+        # add property for options object to to options.options
+        setattr(options.Options, cls._meta.object_name, property(lambda x: cls.singleton.get()))
 
 models.signals.class_prepared.connect(options_class_prepared)
