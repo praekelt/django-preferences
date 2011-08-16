d7bfcd81196b828f50f944536c65914baecd49e4
Shaun Sephton <shaunsephton@gmail.com>
2011-08-16 12:00:20 +0200

    added test runner

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+class TestRunner(test):
+    def run(self, *args, **kwargs):
+        if self.distribution.install_requires:
+            self.distribution.fetch_build_eggs(self.distribution.install_requires)
+        if self.distribution.tests_require:
+            self.distribution.fetch_build_eggs(self.distribution.tests_require)
+        from runtests import runtests
+        runtests()
 
 setup(
     name='django-preferences',
@@ -8,8 +18,13 @@ setup(
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     license='BSD',
+    test_suite = 'preferences.tests',
+    cmdclass={'test': TestRunner},
     url='http://github.com/praekelt/django-preferences',
     packages = find_packages(),
+    tests_require = [
+        'django',
+    ],
     include_package_data=True,
     classifiers = [
         "Programming Language :: Python",
