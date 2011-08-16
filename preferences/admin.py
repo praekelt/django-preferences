dfc48cd169516a06f48efe55cd152e4e960c41cb
Shaun Sephton <shaunsephton@gmail.com>
2011-08-16 15:15:48 +0200

    first commit

---
+++ preferences/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+
+csrf_protect_m = method_decorator(csrf_protect)
+
+class PreferencesAdmin(admin.ModelAdmin):
+    @csrf_protect_m
+    def changelist_view(self, request, extra_context=None):
+        model = self.model
+        if model.objects.all().count() > 1:
+            return super(PreferencesAdmin, self).changelist_view(request)
+        else:
+            obj = model.singleton.get()
+            return redirect(reverse('admin:preferences_%s_change' % model._meta.module_name, args=(obj.id,)))
