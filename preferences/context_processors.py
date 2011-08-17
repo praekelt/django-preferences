164212bc4749dd45ff6130a1e5b11b1084821ffb
Shaun Sephton <shaunsephton@gmail.com>
2011-08-17 15:13:31 +0200

    docstring

--- preferences/context_processors.py
+++ preferences/context_processors.py
@@ -1,4 +1,7 @@
 from preferences import preferences
 
 def preferences_cp(request):
+    """
+    Adds preferences to template context when used through TEMPLATE_CONTEXT_PROCESSORS setting.
+    """
     return {'preferences': preferences}
