7faadfcd892b36ca45c5e3f67d252178c6974b94
Shaun Sephton <shaunsephton@gmail.com>
2011-08-17 15:10:56 +0200

    fixes and cleanup

--- runtests.py
+++ runtests.py
@@ -1,26 +1,30 @@
 import sys
+
 from django.conf import settings
- 
+
 if not settings.configured:
     settings.configure(
         DATABASE_ENGINE='sqlite3',
         INSTALLED_APPS=[
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
             'django.contrib.sites',
-            'preferences.tests',
             'preferences',
+            'preferences.tests',
 
         ],
         TEMPLATE_CONTEXT_PROCESSORS=(
             'preferences.context_processors.preferences_cp',
         ),
-        SITE_ID=1,
+        ROOT_URLCONF='preferences.tests.urls',
     )
  
 from django.test.simple import run_tests
  
-def runtests(*test_args):
-    failures = run_tests(test_args, verbosity=1, interactive=True)
+def runtests():
+    failures = run_tests(('preferences',), verbosity=1, interactive=True)
     sys.exit(failures)
  
 if __name__ == '__main__':
-    runtests(*sys.argv[1:])
+    runtests()
